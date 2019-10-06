2d39f8ed160f1dd8fb5eeab8d1c69e2991bc1dfc
Anatolna <taotoj@gmail.com>
2019-10-06 18:40:47 +0300

    updated columns in bd

--- db_2.py
+++ db_2.py
@@ -2,18 +2,23 @@ import os
 import sqlalchemy
 import sys
 
+from datetime import datetime
 from sqlalchemy import create_engine
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 #from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
 class Posts(Base):
-    __tablename__ = 'VK_posts'
-    user_id = Column(Integer, primary_key=True)
-    r_mine = Column(String)
-    r_wall = Column(String)
+    __tablename__ = 'VK_posts_comments'
+    id_post = Column(Integer, primary_key=True)
+    post = Column(String)
+    id_comm = Column(Integer)
+    comment = Column(String)
+    num_likes = Column(Integer)
+    date = Column(DateTime)
 
-engine = create_engine('sqlite:///posts.db')    
+
+engine = create_engine('sqlite:///vk_posts.db')
 Base.metadata.create_all(engine)
--- db_insert.py
+++ db_insert.py
@@ -1,14 +1,15 @@
+from datetime import datetime
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from db_2 import Base, Posts
 
-engine = create_engine('sqlite:///posts.db')
+engine = create_engine('sqlite:///vk_posts.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-new_post = Posts(r_mine = 'abcde', r_wall = 'qwertyuio')
+new_post = Posts(id_post = 123, post = 'qwertyuio', id_comm = 777, comment = 'фываолдж', num_likes = 5, date = datetime.now())
 session.add(new_post)
 session.commit()
