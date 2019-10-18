24dbdd7e15f524693286e2a75b220025f07265f2
Anatolna <taotoj@gmail.com>
2019-10-18 19:16:14 +0300

    выгружаются больше 100 постов и комментов с post_id

--- posts_comm_oda.py
+++ posts_comm_oda.py
@@ -1,9 +1,6 @@
-# import vk
 import requests
 from datetime import datetime
 import time
-
-
 import settings
 
 
@@ -11,8 +8,6 @@ access_token = settings.access_token
 api_version = 5.101
 offset = 0
 count = 100
-# session = vk.Session(access_token=access_token)
-# api = vk.API(session, v=api_version)
 # domain = input('Введите домен сообщества: ')
 domain = 'tele2'
 
@@ -20,27 +15,23 @@ domain = 'tele2'
 def posts_collector(access_token, api_version, offset, count, domain):
     posts = []
     # try:
-    req_wall = requests.get('https://api.vk.com/method/wall.get', {
-                        'domain': domain,
-                        'offset': offset,
-                        'count': count,  # count - кол-во постов на выходе
-                        'access_token': access_token,
-                        'v': api_version
-                        })
-    # except (requests.RequestException, ValueError, TypeError):
-    # print('Ошибка ввода. Перепроверьте, что введенный домен существует')
+    for offset in range(0, 500, 100):  # добавила цикл, чтобы получать больше, чем 100 постов
+        req_wall = requests.get('https://api.vk.com/method/wall.get', {
+                    'domain': domain,
+                    'offset': offset,
+                    'count': count,  # count - кол-во постов на выходе
+                    'access_token': access_token,
+                    'v': api_version
+                    })
+    # except(requests.ConnectionError):
+    #     print('Сетевая ошибка')
+    #     return False
+    # return False
     # print(req_wall.json()["response"]['items'])
     # member_count = req_wall.json()['response']['count']
-    # print("total", member_count)
-    for offset in range(0, 222, 100):
-        req_wall = requests.get('https://api.vk.com/method/wall.get', {
-                        'domain': domain,
-                        'offset': offset,
-                        'count': count,  # count - кол-во постов на выходе
-                        'access_token': access_token,
-                        'v': api_version
-                        })
-        # print(offset)
+    # print("total members = ", member_count)
+    # except (requests.RequestException, ValueError, TypeError):
+        # print('Ошибка ввода. Перепроверьте, что введенный домен существует')
         time.sleep(0.5)
         for post in req_wall.json()['response']['items']:
             posts.append({
@@ -48,19 +39,19 @@ def posts_collector(access_token, api_version, offset, count, domain):
                 'text': post['text'],
                 'date': datetime.fromtimestamp(post['date']).strftime('%d/%m/%y %H:%M')
                 })
-    return posts
-    # print(len(posts))
+    print('len posts = ', len(posts))
     # print(posts)
+    return posts
 
 
 posts_collector(access_token, api_version, offset, count, domain)
 
 
-check_inputed = requests.get('https://api.vk.com/method/utils.resolveScreenName',{
+check_inputed = requests.get('https://api.vk.com/method/utils.resolveScreenName', {
                     'screen_name': domain,
                     'access_token': access_token,
                     'v': api_version,
-                        })
+                    })
 owner_id = 0 - (check_inputed.json()["response"]['object_id'])
 # print(owner_id)
 
@@ -71,31 +62,30 @@ count_comm = 100
 
 def comments_collector(post, access_token, api_version, offset, count_comm, domain, owner_id):
     comments = []
-    req_comms = requests.get('https://api.vk.com/method/wall.getComments', {
-                        'domain': domain,
-                        'offset': offset,
-                        'count': count_comm,  # у комментов свой count
-                        'access_token': access_token,
-                        'v': api_version,
-                        'post_id': post,
-                        'owner_id': owner_id,
-                        })
+    for offset in range(0, 300, 100):
+        req_comms = requests.get('https://api.vk.com/method/wall.getComments', {
+                            'domain': domain,
+                            'offset': offset,
+                            'count': count_comm,  # у комментов свой count!
+                            'access_token': access_token,
+                            'v': api_version,
+                            'post_id': post,
+                            'owner_id': owner_id,
+                            })
     # print(req_comms.json()["response"]['items'])
-    for comms in req_comms.json()['response']['items']:
-        if 'deleted' in comms.keys():
-            print("DELETED posted")
-        else:
-            # print(comms)
-            comments.append({
-                'post_id': comms['post_id'],
-                'comms': comms['text'],
-                'date': datetime.fromtimestamp(comms['date']).strftime('%d/%m/%y %H:%M')
-                })
+        time.sleep(0.5)
+        for comms in req_comms.json()['response']['items']:
+            if 'post_id' in comms.keys():  # берем только комменты, где есть post_id (и текст)
+                comments.append({
+                    'post_id': comms['post_id'],
+                    'comms': comms['text'],
+                    'date': datetime.fromtimestamp(comms['date']).strftime('%d/%m/%y %H:%M')
+                    })
     # print(comments)
-    # print(len(comments))
+    print(f'len comments = {len(comments)}')
     return comments
 
-# wow = (554689, 555788, 556333)
+# wow = (565886, 554689, 555788, 556333)
 # for i in wow:
 #     post = i
 
@@ -103,4 +93,5 @@ def comments_collector(post, access_token, api_version, offset, count_comm, doma
 comments_collector(post, access_token, api_version, offset, count_comm, domain, owner_id)
 
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
+    print('все отработано')
